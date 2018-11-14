41f1a64560eaf520907eabbdb709da7bec7b3f71
tsahom <thomas.palis@gmail.com>
2018-11-14 12:20:49 +0100

    Vrai ajout de passagerStandard

---
+++ src/tec/PassagerStandard.java
@@ -0,0 +1,60 @@
+package tec;
+
+public class PassagerStandard implements Passager, Usager{
+
+	@Override
+	public void monterDans(Transport t) throws UsagerInvalideException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String nom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean estDehors() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean estAssis() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean estDebout() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void accepterSortie() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accepterPlaceAssise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accepterPlaceDebout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nouvelArret(Bus bus, int numeroArret) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
