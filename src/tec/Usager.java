6bb9bbb2932dbc3e748402c291ee8a05a45e8de0
tsahom <thomas.palis@gmail.com>
2018-11-14 11:33:35 +0100

    Init commit

---
+++ src/tec/Usager.java
@@ -0,0 +1,26 @@
+package tec;
+
+/**
+ * Cette interface définit le type manipulé par le programme principal.
+ * <p> 
+ * Suivant son caractère, l'usager choisit a la montée une place assise, 
+ * debout ou reste dehors.
+ * Le transport est chargé d'authoriser cette demande. Si le transport
+ * refuse toute demande, le passager reste dehors.
+ */
+public interface Usager {
+  /**
+   * fournit le nom de l'usager.
+   */
+  public String nom();
+
+  /**
+   * Fournit à l'usager le transport auquel il peut accéder.
+   * Cette méthode réalise le caractère du passager à la montée.
+   * 
+   * @param t le transport dans lequel désire monter l'usager.
+   * @throws si l'état de l'usager est incohérent par rapport à sa demande.
+   */
+  public void monterDans(Transport t) throws UsagerInvalideException;
+}
+
