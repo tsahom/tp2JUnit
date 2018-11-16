b05f30100726294df4c5e8a6d42a63022aa3475f
tsahom <thomas.palis@gmail.com>
2018-11-16 16:34:12 +0100

    Implementation des methodes et des tests qui ne communique pas avec la classe Autobus de PassagerStandard

--- src/test/PassagerStandardTest.java
+++ src/test/PassagerStandardTest.java
@@ -5,15 +5,86 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import tec.PassagerStandard;
+import tec.Transport;
+import tec.UsagerInvalideException;
+
 class PassagerStandardTest {
 
+	PassagerStandard debout;
+	PassagerStandard assis;
+	PassagerStandard dehors;
+	Transport t;
 	@BeforeEach
 	void setUp() throws Exception {
+		debout = new PassagerStandard("Michel", 4);
+		debout.accepterPlaceDebout();
+		assis = new PassagerStandard("Suzie",3);
+		assis.accepterPlaceAssise();
+		dehors = new PassagerStandard("Luc", 5);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testNom() {
+		assertEquals("Michel",debout.nom());
+		assertNotEquals("Suzie",debout.nom());
+		assertEquals("Suzie",assis.nom());
+		assertNotEquals("Michel",assis.nom());
+		assertEquals("Luc",dehors.nom());
+		assertNotEquals("Michel",dehors.nom());
+		assertNotEquals("Test",debout.nom());
 	}
+	
+	@Test
+	void testEstDehors() {
+		assertFalse(debout.estDehors());
+		assertFalse(assis.estDehors());
+		assertTrue(dehors.estDehors());
+	}
+	
+	@Test
+	void testEstAssis() {
+		assertFalse(debout.estAssis());
+		assertTrue(assis.estAssis());
+		assertFalse(dehors.estAssis());
+	}
+
+	@Test
+	void testEstDebout() {
+		assertTrue(debout.estDebout());
+		assertFalse(assis.estDebout());
+		assertFalse(dehors.estDebout());
+	}
+	
+	@Test
+	void testAccepterSortie() {
+		assertFalse(debout.estDehors());
+		debout.accepterSortie();
+		assertTrue(debout.estDehors());
+	}
+	
+	@Test
+	void testAccepterPlaceAssie() {
+		assertFalse(debout.estAssis());
+		debout.accepterPlaceAssise();
+		assertTrue(debout.estAssis());
+	}
+	
+	@Test
+	void testAccepterPlaceDebout() {
+		assertFalse(assis.estDebout());
+		assis.accepterPlaceDebout();
+		assertTrue(assis.estDebout());
+	}
+	
+	@Test
+	void testToString() {
+		assertEquals("Michel debout",debout.toString());
+		assertEquals("Suzie assis",assis.toString());
+		assertEquals("Luc dehors",dehors.toString());
+	}
+	
+	
+	
 
 }
