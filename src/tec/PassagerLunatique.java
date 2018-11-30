35adfda5f0048d51839b97852464cb407996d37e
etu <bourgon.nils@gmail.com>
2018-11-30 17:14:58 +0100

    Ajout classe Lunatique

---
+++ src/tec/PassagerLunatique.java
@@ -0,0 +1,20 @@
+package tec;
+
+public class PassagerLunatique extends PassagerAbstrait{
+
+	public PassagerLunatique(String nom,int arret) {
+		super(nom, arret);
+	}
+	
+	
+	@Override
+	public void nouvelArret(Bus bus, int numeroArret) throws UsagerInvalideException {
+		
+	}
+
+	@Override
+	public void monterDans(Transport t) throws UsagerInvalideException {
+		
+	}
+
+}
